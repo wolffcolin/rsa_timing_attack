7242ab36745117f618cccb569ff78f4c8a91b4e4
axel adams <axeladams@gmail.com>
2026-04-26 12:33:45 -0600

    Timing in the neighborhood

--- Cracking.c
+++ Cracking.c
@@ -8,25 +8,26 @@
 #include <stdlib.h>
 //#include "eea.c"
 
-#define ITERATIONS 10000
-#define NEIGHBORHOOD 1
+#define ITERATIONS 100
+#define NEIGHBORHOOD 512
 
 //#define DEBUG
 
 
-
+// A compare algorithm for quicksort
 int compare_long_long(const void *a, const void *b) {
-    const long long *val_a = (const long long *)a;
-    const long long *val_b = (const long long *)b;
+    const unsigned long long *val_a = (const unsigned long long *)a;
+    const unsigned long long *val_b = (const unsigned long long *)b;
 
     if (*val_a < *val_b) return -1;
     if (*val_a > *val_b) return 1;
     return 0;
 }
 
+
 int main()
 {
-	RSA *rsa = NULL;
+	RSA *rsa;
 	// Read the private key file
 	FILE *fp = fopen("private_key.pem", "r");
 	if (!fp) {
@@ -58,10 +59,9 @@ int main()
 	BIGNUM *R = BN_new();
 	BIGNUM *Rprime = BN_new();
 	BIGNUM *temp = BN_new();
-	BN_lshift(R, BN_value_one(), mont->ri);
+        BN_set_bit(R, mont->ri);
 	BN_mod_inverse(Rprime, R, rsa->n, ctx);
         printf("Montgomery R bits: %d\n", mont->ri);
-
 
 	printf("R: ");
 	BN_print_fp(stdout, R);
@@ -77,35 +77,41 @@ int main()
 	printf("\n");
         BN_free(temp);
 
+
 	// Allocate space for input and output of decryption
 	int rsa_size = RSA_size(rsa);
 	printf("RSA Size is %d\n", rsa_size);
 	unsigned char ciphertext[rsa_size];
 	unsigned char decrypted[rsa_size];
 
-	printf("\nq:     ");
-	BN_print_fp(stdout, rsa->q);
-	printf("\n");
+
 
 	BIGNUM *guess = BN_new();
         BIGNUM *guessneighbor = BN_new();
         BIGNUM *cipherguess = BN_new();
+
+        // Instantiate guess with the top hundred or so bits of q
 	BN_rshift(guess, rsa->q, 257);
 	BN_lshift(guess, guess, 257);
 
-	printf("guess: ");
+        printf("\nq:             ");
+	BN_print_fp(stdout, rsa->q);
+	printf("\n");
+
+	printf("guess:         ");
 	BN_print_fp(stdout, guess);
 	printf("\n");
 
+
+        // For storing timings
+       	unsigned long long cycles[NEIGHBORHOOD][ITERATIONS] = {0};
 
 // *********************************************************************************************
 //      This is the start of actual decryption
 //
 //
-	long long cycles[NEIGHBORHOOD][ITERATIONS] = {0};
 	// Loop for timings
-	for (unsigned long variations = 0; variations < 1; variations++) {
-		memset(ciphertext, 0, rsa_size);
+	for (unsigned long variations = 0; variations < NEIGHBORHOOD; variations++) {
                 BN_zero(guessneighbor);
                 BN_set_word(guessneighbor, variations);
                 BN_add(guessneighbor, guess, guessneighbor);
@@ -115,15 +121,17 @@ int main()
                 BN_print_fp(stdout, guessneighbor);
                 printf("\n");
 
+                // Write cipherguess to ciphertext array (0 padded)
+                memset(ciphertext, 0, (rsa_size - BN_num_bytes(cipherguess)));
 		int bytes_written = BN_bn2bin(
 			cipherguess, ciphertext + (rsa_size - BN_num_bytes(cipherguess)));
-#ifdef DEBUG
+// #ifdef DEBUG
 			printf("\n Ciphertext: ");
 			for (int i = 0; i < rsa_size; i++) {
 				printf("%02x", ciphertext[i]);
 			}
 			printf("\n");
-#endif
+// #endif
 
 		for (int i = 0; i < ITERATIONS; i++) {
 			// Timing code from https://blog.codingconfessions.com/p/rdtsc
@@ -150,7 +158,7 @@ int main()
 			if (result == -1) {
 				ERR_print_errors_fp(stderr);
 			} else {
-				cycles[0][i] = (end - start);
+				cycles[variations][i] = (end - start);
 #ifdef DEBUG
 				printf("Decryption successful!\n");
 				printf("Cycles taken: %llu\n", (end - start));
@@ -159,150 +167,150 @@ int main()
 			}
 		}
 	}
-        qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(long long), compare_long_long);
-	printf("cycles: %d, %d, %d\n", cycles[0][0], cycles[0][50], cycles[0][ITERATIONS-1]);
+        qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(unsigned long long), compare_long_long);
+	printf("cycles: %llu, %llu, %llu\n", ((unsigned long long *) cycles)[0], ((unsigned long long *) cycles)[ITERATIONS*NEIGHBORHOOD/2], ((unsigned long long *) cycles)[ITERATIONS*NEIGHBORHOOD-1]);
 
 
-// *********************************************************************************************
-//      Second Guess
-//
-//
-	BN_rshift(guess, rsa->q, 256);
-	BN_lshift(guess, guess, 256);
+// // *********************************************************************************************
+// //      Second Guess
+// //
+// //
+// 	BN_rshift(guess, rsa->q, 256);
+// 	BN_lshift(guess, guess, 256);
 
-	printf("guess: ");
-	BN_print_fp(stdout, guess);
-	printf("\n");
-	for (unsigned long variations = 0; variations < 1; variations++) {
-		memset(ciphertext, 0, rsa_size);
-                BN_zero(guessneighbor);
-                BN_set_word(guessneighbor, variations);
-                BN_add(guessneighbor, guess, guessneighbor);
-                BN_mod_mul(cipherguess, guessneighbor, Rprime, rsa->n, ctx);
+// 	printf("guess: ");
+// 	BN_print_fp(stdout, guess);
+// 	printf("\n");
+// 	for (unsigned long variations = 0; variations < 1; variations++) {
+// 		memset(ciphertext, 0, rsa_size);
+//                 BN_zero(guessneighbor);
+//                 BN_set_word(guessneighbor, variations);
+//                 BN_add(guessneighbor, guess, guessneighbor);
+//                 BN_mod_mul(cipherguess, guessneighbor, Rprime, rsa->n, ctx);
 
-                printf("guessneighbor: ");
-                BN_print_fp(stdout, guessneighbor);
-                printf("\n");
+//                 printf("guessneighbor: ");
+//                 BN_print_fp(stdout, guessneighbor);
+//                 printf("\n");
 
-		int bytes_written = BN_bn2bin(
-			cipherguess, ciphertext + (rsa_size - BN_num_bytes(cipherguess)));
-#ifdef DEBUG
-			printf("\n Ciphertext: ");
-			for (int i = 0; i < rsa_size; i++) {
-				printf("%02x", ciphertext[i]);
-			}
-			printf("\n");
-#endif
+// 		int bytes_written = BN_bn2bin(
+// 			cipherguess, ciphertext + (rsa_size - BN_num_bytes(cipherguess)));
+// #ifdef DEBUG
+// 			printf("\n Ciphertext: ");
+// 			for (int i = 0; i < rsa_size; i++) {
+// 				printf("%02x", ciphertext[i]);
+// 			}
+// 			printf("\n");
+// #endif
 
-		for (int i = 0; i < ITERATIONS; i++) {
-			// Timing code from https://blog.codingconfessions.com/p/rdtsc
-			unsigned int auxCPUID1, auxCPUID2;
-			unsigned long long start, end;
-			_mm_lfence();
-			start = __rdtscp(&auxCPUID1);
-			// Decrypt
-			// RSA_NO_PADDING as used in the paper
-			int result = RSA_private_decrypt(rsa_size, ciphertext,
-							 decrypted, rsa,
-							 RSA_NO_PADDING);
-			_mm_lfence();
-			end = __rdtscp(&auxCPUID2);
+// 		for (int i = 0; i < ITERATIONS; i++) {
+// 			// Timing code from https://blog.codingconfessions.com/p/rdtsc
+// 			unsigned int auxCPUID1, auxCPUID2;
+// 			unsigned long long start, end;
+// 			_mm_lfence();
+// 			start = __rdtscp(&auxCPUID1);
+// 			// Decrypt
+// 			// RSA_NO_PADDING as used in the paper
+// 			int result = RSA_private_decrypt(rsa_size, ciphertext,
+// 							 decrypted, rsa,
+// 							 RSA_NO_PADDING);
+// 			_mm_lfence();
+// 			end = __rdtscp(&auxCPUID2);
 
-#ifdef DEBUG
-			printf("\nPlaintext: ");
-			for (int i = 0; i < rsa_size; i++) {
-				printf("%02x ", decrypted[i]);
-			}
-			printf("\n");
-#endif
+// #ifdef DEBUG
+// 			printf("\nPlaintext: ");
+// 			for (int i = 0; i < rsa_size; i++) {
+// 				printf("%02x ", decrypted[i]);
+// 			}
+// 			printf("\n");
+// #endif
 
-			if (result == -1) {
-				ERR_print_errors_fp(stderr);
-			} else {
-				cycles[0][i] = (end - start);
-#ifdef DEBUG
-				printf("Decryption successful!\n");
-				printf("Cycles taken: %llu\n", (end - start));
-				printf("CPUIDs: %d, %d\n", auxCPUID1, auxCPUID2);
-#endif
-			}
-		}
-	}
-        qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(long long), compare_long_long);
-	printf("cycles: %d, %d, %d\n", cycles[0][0], cycles[0][50], cycles[0][ITERATIONS-1]);
-
-
-// *********************************************************************************************
-//      Third Guess
-//
-//
-	BN_rshift(guess, rsa->q, 256);
-	BN_lshift(guess, guess, 1);
-        BN_one(guessneighbor);
-        BN_add(guess, guess, guessneighbor);
-        BN_lshift(guess, guess, 255);
+// 			if (result == -1) {
+// 				ERR_print_errors_fp(stderr);
+// 			} else {
+// 				cycles[0][i] = (end - start);
+// #ifdef DEBUG
+// 				printf("Decryption successful!\n");
+// 				printf("Cycles taken: %llu\n", (end - start));
+// 				printf("CPUIDs: %d, %d\n", auxCPUID1, auxCPUID2);
+// #endif
+// 			}
+// 		}
+// 	}
+//         qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(long long), compare_long_long);
+// 	printf("cycles: %d, %d, %d\n", cycles[0][0], cycles[0][50], cycles[0][ITERATIONS-1]);
 
 
-	printf("guess: ");
-	BN_print_fp(stdout, guess);
-	printf("\n");
-	for (unsigned long variations = 0; variations < 1; variations++) {
-		memset(ciphertext, 0, rsa_size);
-                BN_zero(guessneighbor);
-                BN_set_word(guessneighbor, variations);
-                BN_add(guessneighbor, guess, guessneighbor);
-                BN_mod_mul(cipherguess, guessneighbor, Rprime, rsa->n, ctx);
+// // *********************************************************************************************
+// //      Third Guess
+// //
+// //
+// 	BN_rshift(guess, rsa->q, 256);
+// 	BN_lshift(guess, guess, 1);
+//         BN_one(guessneighbor);
+//         BN_add(guess, guess, guessneighbor);
+//         BN_lshift(guess, guess, 255);
 
-                printf("guessneighbor: ");
-                BN_print_fp(stdout, guessneighbor);
-                printf("\n");
 
-		int bytes_written = BN_bn2bin(
-			cipherguess, ciphertext + (rsa_size - BN_num_bytes(cipherguess)));
-#ifdef DEBUG
-			printf("\n Ciphertext: ");
-			for (int i = 0; i < rsa_size; i++) {
-				printf("%02x", ciphertext[i]);
-			}
-			printf("\n");
-#endif
+// 	printf("guess: ");
+// 	BN_print_fp(stdout, guess);
+// 	printf("\n");
+// 	for (unsigned long variations = 0; variations < 1; variations++) {
+// 		memset(ciphertext, 0, rsa_size);
+//                 BN_zero(guessneighbor);
+//                 BN_set_word(guessneighbor, variations);
+//                 BN_add(guessneighbor, guess, guessneighbor);
+//                 BN_mod_mul(cipherguess, guessneighbor, Rprime, rsa->n, ctx);
 
-		for (int i = 0; i < ITERATIONS; i++) {
-			// Timing code from https://blog.codingconfessions.com/p/rdtsc
-			unsigned int auxCPUID1, auxCPUID2;
-			unsigned long long start, end;
-			_mm_lfence();
-			start = __rdtscp(&auxCPUID1);
-			// Decrypt
-			// RSA_NO_PADDING as used in the paper
-			int result = RSA_private_decrypt(rsa_size, ciphertext,
-							 decrypted, rsa,
-							 RSA_NO_PADDING);
-			_mm_lfence();
-			end = __rdtscp(&auxCPUID2);
+//                 printf("guessneighbor: ");
+//                 BN_print_fp(stdout, guessneighbor);
+//                 printf("\n");
 
-#ifdef DEBUG
-			printf("\nPlaintext: ");
-			for (int i = 0; i < rsa_size; i++) {
-				printf("%02x ", decrypted[i]);
-			}
-			printf("\n");
-#endif
+// 		int bytes_written = BN_bn2bin(
+// 			cipherguess, ciphertext + (rsa_size - BN_num_bytes(cipherguess)));
+// #ifdef DEBUG
+// 			printf("\n Ciphertext: ");
+// 			for (int i = 0; i < rsa_size; i++) {
+// 				printf("%02x", ciphertext[i]);
+// 			}
+// 			printf("\n");
+// #endif
 
-			if (result == -1) {
-				ERR_print_errors_fp(stderr);
-			} else {
-				cycles[0][i] = (end - start);
-#ifdef DEBUG
-				printf("Decryption successful!\n");
-				printf("Cycles taken: %llu\n", (end - start));
-				printf("CPUIDs: %d, %d\n", auxCPUID1, auxCPUID2);
-#endif
-			}
-		}
-	}
-        qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(long long), compare_long_long);
-	printf("cycles: %d, %d, %d\n", cycles[0][0], cycles[0][50], cycles[0][ITERATIONS-1]);
+// 		for (int i = 0; i < ITERATIONS; i++) {
+// 			// Timing code from https://blog.codingconfessions.com/p/rdtsc
+// 			unsigned int auxCPUID1, auxCPUID2;
+// 			unsigned long long start, end;
+// 			_mm_lfence();
+// 			start = __rdtscp(&auxCPUID1);
+// 			// Decrypt
+// 			// RSA_NO_PADDING as used in the paper
+// 			int result = RSA_private_decrypt(rsa_size, ciphertext,
+// 							 decrypted, rsa,
+// 							 RSA_NO_PADDING);
+// 			_mm_lfence();
+// 			end = __rdtscp(&auxCPUID2);
+
+// #ifdef DEBUG
+// 			printf("\nPlaintext: ");
+// 			for (int i = 0; i < rsa_size; i++) {
+// 				printf("%02x ", decrypted[i]);
+// 			}
+// 			printf("\n");
+// #endif
+
+// 			if (result == -1) {
+// 				ERR_print_errors_fp(stderr);
+// 			} else {
+// 				cycles[0][i] = (end - start);
+// #ifdef DEBUG
+// 				printf("Decryption successful!\n");
+// 				printf("Cycles taken: %llu\n", (end - start));
+// 				printf("CPUIDs: %d, %d\n", auxCPUID1, auxCPUID2);
+// #endif
+// 			}
+// 		}
+// 	}
+//         qsort(cycles, ITERATIONS * NEIGHBORHOOD, sizeof(long long), compare_long_long);
+// 	printf("cycles: %d, %d, %d\n", cycles[0][0], cycles[0][50], cycles[0][ITERATIONS-1]);
 	
 
 
